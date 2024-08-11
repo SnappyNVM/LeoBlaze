8246130329224c377468c4146ea3bbb559c9009b
Artem <evdafe@gmail.com>
2024-08-11 16:52:01 +0300

    +Keys

---
+++ Assets/Scripts/PlayerPrefsKeys.cs
@@ -0,0 +1,7 @@
+public static class PlayerPrefsKeys
+{
+    public readonly static string ThemeKey = "Theme";
+    public readonly static string DarkKey = "DarkKey";
+    public readonly static string LightKey = "LightKey";
+    public readonly static string Language = "Language";
+}
