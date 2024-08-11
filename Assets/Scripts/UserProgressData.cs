7f7757bb4003bb7f4b0eb3ff9e08b5b28cd5349a
SnappyNVM <112257868+SnappyNVM@users.noreply.github.com>
2024-08-11 18:51:26 +0500

    +scriptsAmostFixed

--- Assets/Scripts/UserProgressData.cs
+++ Assets/Scripts/UserProgressData.cs
@@ -1,27 +1,8 @@
 using Assets.Scripts;
-using NoobsMuc.Coinmarketcap.Client;
 using System;
 using System.Collections.Generic;
 
 [Serializable]
 public class UserProgressData
 {
-    public Dictionary<CurrencyType, CurrencyPurchaseData> CurrencyDatas = new()
-    {
-        [CurrencyType.BTC] = new CurrencyPurchaseData() { _currency= CurrencyType.BTC, _dollarPurchase = 0, _dollarPurchasePrice = 0 },
-        [CurrencyType.RUB] = new CurrencyPurchaseData() { _currency = CurrencyType.RUB, _dollarPurchase = 0, _dollarPurchasePrice = 0 },
-        [CurrencyType.USD] = new CurrencyPurchaseData() { _currency = CurrencyType.USD, _dollarPurchase = 0, _dollarPurchasePrice = 0 },
-        [CurrencyType.DAO] = new CurrencyPurchaseData() { _currency = CurrencyType.DAO, _dollarPurchase = 0, _dollarPurchasePrice = 0 },
-        [CurrencyType.ETH] = new CurrencyPurchaseData() { _currency = CurrencyType.ETH, _dollarPurchase = 0, _dollarPurchasePrice = 0 }
-    };
-    public Dictionary<CurrencyType, Currency> Currencies = new()
-    {
-        [CurrencyType.BTC] = new Currency(),
-        [CurrencyType.RUB] = new Currency(),
-        [CurrencyType.USD] = new Currency(),
-        [CurrencyType.DAO] = new Currency(),
-        [CurrencyType.ETH] = new Currency()
-    };
-    public bool FirstSession = true;
-    public DateTime PrevUpdateTime;
 }
